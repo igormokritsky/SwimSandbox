a3340ad7312a994a54c7ba289ff37eed40fab735
Igor Mokritsky <igormokritsky@gmail.com>
2020-04-29 17:53:37 +0300

    New Update!

---
+++ src/main/java/org/igormokritsky/servlets/coachServlet.java
@@ -0,0 +1,35 @@
+package org.igormokritsky.servlets;
+
+import org.igormokritsky.DAOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+public class coachServlet extends HttpServlet {
+
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+
+
+
+
+}
